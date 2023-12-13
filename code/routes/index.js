1c6f6709ca8dcd8c6c50ecd3c1d4df09c6b97849
juliapieters <piet0058@hz.nl>
2023-12-13 09:49:45 +0100

    first login changes

--- code/routes/index.js
+++ code/routes/index.js
@@ -6,40 +6,41 @@ import {
   getSingleAccount,
   makeNewAccount,
 } from '../controllers/accountsController.js';
+import { createUser } from '../lib/server/db/index.js';
 const router = express.Router();
 
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({ extended: false }));
+// router.use(bodyParser.json());
+// router.use(bodyParser.urlencoded({ extended: false }));
 
 // routes
 router.get('/', (req, res, next) => {
   res.json('Hi, this is the accounts microservice');
 });
-router.post('/', cors(), makeNewAccount);
+// router.post('/', cors(), makeNewAccount);
 
 //router.post('/', cors(), (req, res) => {
 //  res.send('POST request to the accounts microservice')
 //});
 
-router.options('/accounts', (req, res, next) => {
-  try {
-    //set header before response
-    res.header({
-      allow: 'GET, POST, OPTIONS',
-      'Content-type': 'application/json',
-      Data: Date.now(),
-      'Content-length': 0,
-    });
-    //response
-    res.sendStatus(200);
-  } catch (err) {
-    next(err);
-  }
-});
+// router.options('/accounts', (req, res, next) => {
+//   try {
+//     //set header before response
+//     res.header({
+//       allow: 'GET, POST, OPTIONS',
+//       'Content-type': 'application/json',
+//       Data: Date.now(),
+//       'Content-length': 0,
+//     });
+//     //response
+//     res.sendStatus(200);
+//   } catch (err) {
+//     next(err);
+//   }
+// });
 
 // get a collection of all the accounts, you can also use a query
-router.get('/accounts', cors(), getAllAccounts);
-router.get('/accounts/:id', cors(), getSingleAccount);
-//router.post('/accounts', cors(), setAccounts);
+// router.get('/accounts', cors(), getAllAccounts);
+// router.get('/accounts/:id', cors(), getSingleAccount);
+// router.post('/accounts', cors(), createUser);
 
 export default router;
